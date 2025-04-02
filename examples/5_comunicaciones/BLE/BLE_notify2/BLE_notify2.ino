045eedd6b758eaffd51f98ef80e3602bb716d0c0
JaviVi <jvicente@deusto.es>
2025-04-02 09:27:58 +0200

    ble 3 caracteristicas

---
+++ examples/5_comunicaciones/BLE/BLE_notify2/BLE_notify2.ino
@@ -0,0 +1,125 @@
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 4fafc201-1fb5-459e-8fcc-c5c9c331914b
+   And has a characteristic of: beb5483e-36e1-4688-b7f5-ea07361b26a8
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   A connect hander associated with the server starts a background task that performs notification
+   every couple of seconds.
+*/
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLECharacteristic *pCharacteristic1;
+BLECharacteristic *pCharacteristic2;
+BLECharacteristic *pCharacteristic3;
+
+bool deviceConnected = false;
+uint8_t value = 0;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID          "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID1  "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUID2  "beb5483e-36e1-4688-b7f5-ea07361b26a9"
+#define CHARACTERISTIC_UUID3  "beb5483e-36e1-4688-b7f5-ea07361b26aA"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        for (int i = 0; i < rxValue.length(); i++)
+          Serial.print(rxValue[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+
+
+void setup() {
+  Serial.begin(115200);
+
+  // Create the BLE Device
+  BLEDevice::init("MyESP32");
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic1 = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID1,
+                       BLECharacteristic::PROPERTY_READ   
+                     );
+  pCharacteristic2 = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID2,
+                       BLECharacteristic::PROPERTY_WRITE |
+                       BLECharacteristic::PROPERTY_READ 
+                     );
+  pCharacteristic3 = pService->createCharacteristic(
+                       CHARACTERISTIC_UUID3,
+                       BLECharacteristic::PROPERTY_READ   |
+                       BLECharacteristic::PROPERTY_NOTIFY |
+                       BLECharacteristic::PROPERTY_INDICATE
+                     );
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pCharacteristic1->addDescriptor(new BLE2902());
+  pCharacteristic2->addDescriptor(new BLE2902());
+  pCharacteristic3->addDescriptor(new BLE2902());
+
+  pCharacteristic2->setCallbacks(new MyCallbacks());
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+
+  if (deviceConnected) {
+    Serial.printf("*** NOTIFY: %d ***\n", value);
+    pCharacteristic1->setValue("holaMundo\n");
+    String valueTexto = (String) value;
+    pCharacteristic3->setValue(valueTexto.c_str());
+    pCharacteristic3->notify();
+    //pCharacteristic->indicate();
+    value++;
+  }
+  delay(2000);
+}
